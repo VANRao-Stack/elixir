94466ae52d73e6d8d4aff36b2f99716a6fd66c68
V Abhijith Rao <varsg007@gmail.com>
2021-02-08 12:06:05 +0530

    Update utils.py

--- elixir/utils.py
+++ elixir/utils.py
@@ -8,7 +8,9 @@ def plot(network, z, t, type_plot, num_test_samples=1000):
   print('NOTE: The x axis represents the z value, the y axis, the t value and finally the z axis the predicted value of flow/radii.')
   z_flat = np.linspace(z[0], z[1], num_test_samples)
   t_flat = np.linspace(t[0], t[1], num_test_samples)
-  u = network.predict([z_flat, t_flat], batch_size=num_test_samples)
+  u = []
+  for i in range(num_test_samples):
+    u.append(network.predict(np.asarray([[z_flat[i], t_flat[i]]])))
   fig = go.Figure(data=[go.Surface(x=z_flat, y=t_flat, z=u)])
   print('NOTE: The x-axis represents the location along the artery,\n the y-axis, the time of evaluation and the z-axis the \n predicted value of {}'.format(type_plot)) 
   fig.update_layout(title='Plot of {}'.format(type_plot))
