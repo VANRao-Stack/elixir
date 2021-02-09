6c20f0087b62ee92b65c5d15bd9877a88bbc2f89
SanaulMalik <68287047+SanaulMalik@users.noreply.github.com>
2021-02-10 01:41:54 +0300

    Update v1.py

--- elixir app/v1.py
+++ elixir app/v1.py
@@ -1,3 +1,4 @@
+
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
@@ -13,7 +14,7 @@ from kivymd.app import MDApp
 from kivymd.uix.textfield import MDTextField
 from kivymd.uix.button import MDFillRoundFlatButton,MDIconButton
 from kivymd.uix.label import MDIcon
-import elixir as ex
+#import elixir as ex
 
 Builder.load_string("""
 <BackgroundColor@Widget>:
@@ -24,6 +25,42 @@ Builder.load_string("""
         Rectangle:
             size: self.size
             pos: self.pos
+<HomeButton@MDIconButton>:
+    icon:"home-circle-outline"
+    theme_text_color:"Custom"
+    #text_color:(135/255,0,106/255,1)
+    text_color:(1,1,1,1)
+    user_font_size: "80sp"
+    on_release:
+        app.root.current = "input"
+<AboutButton@MDIconButton>:
+    icon:"help"
+    theme_text_color:"Custom"
+    #text_color :(135/255,0,106/255,1)
+    text_color:(1,1,1,1)
+    user_font_size : "80sp"
+
+<SideBar@BoxLayout>:
+    orientation:"vertical"
+    canvas.before:
+        Color:
+            rgba:(0/255,155/255,133/255,1)
+        Rectangle:
+            size:self.size
+            pos:self.pos
+    Label:
+        size_hint:(1,0.3)
+        text:"Elixir"
+        font_size:"30sp"
+    HomeButton:
+        size_hint:(1,.5)
+        #pos_hint:{"center_x":.5,"center_y":.5}
+        #icon:"home-variant-outline"
+    AboutButton:
+        size_hint:(1,0.5)
+
+    BoxLayout:
+
 <ColorLabel@BackgroundColor+Label>:
     background_color:(0/255,155/255,133/255,1)
     color:(1,1,1,1)
@@ -36,35 +73,9 @@ Builder.load_string("""
     timePeriod:timePeriod
     BoxLayout:
         orientation: "horizontal"
-        BoxLayout:
+        SideBar:
             size_hint : 0.3,1
-            orientation:"vertical"
-            canvas.before:
-                Color:
-                    rgba:(0,22/255,112/255,1)
-                Rectangle:
-                    size:self.size
-                    pos:self.pos
-            Label:
-                size_hint:(1,0.3)
-                text:"Elixir"
-                font_size:"30sp"
-            MDIconButton:
-                size_hint:(1,.5)
-                #pos_hint:{"center_x":.5,"center_y":.5}
-                #icon:"home-variant-outline"
-                icon:"home-circle-outline"
-                theme_text_color:"Custom"
-                text_color:(135/255,0,106/255,1)
-                user_font_size: "80sp"
-            MDIconButton:
-                size_hint:(1,0.5)
-                icon:"help"
-                theme_text_color:"Custom"
-                text_color :(135/255,0,106/255,1)
-                user_font_size : "80sp"
-            BoxLayout:
-                
+      
         RelativeLayout:
             Image:
                 source: "./Media/BlueCum.jpg"
@@ -115,7 +126,7 @@ Builder.load_string("""
                     RelativeLayout:
                         MDFillRoundFlatButton:
                             text: "Advanced Settings"
-                            text_size:self.width, None 
+                            #text_size:self.width, None 
                             theme_text_color: "Custom"
                             text_color: 1, 1, 1, 1
                             md_bg_color: (0/255,155/255,133/255,1)
@@ -137,43 +148,158 @@ Builder.load_string("""
     name : "load"
     BoxLayout:
         orientation: "horizontal"
-        BoxLayout:
-            size_hint : 0.3,1
-            orientation:"vertical"
-            canvas.before:
-                Color:
-                    rgba:(0,22/255,112/255,1)
-                Rectangle:
-                    size:self.size
-                    pos:self.pos
-            Label:
-                size_hint:(1,0.3)
-                text:"Elixir"
-            MDIconButton:
-                size_hint:(1,.5)
-                #pos_hint:{"center_x":.5,"center_y":.5}
-                #icon:"home-variant-outline"
-                icon:"home-circle-outline"
-                theme_text_color:"Custom"
-                text_color:(135/255,0,106/255,1)
-                user_font_size: "80sp"
-            MDIconButton:
-                size_hint:(1,0.5)
-                icon:"help"
-                theme_text_color:"Custom"
-                text_color :(135/255,0,106/255,1)
-                user_font_size : "80sp"
-            BoxLayout:
-                
+        SideBar:
+            size_hint : 0.3,1     
         RelativeLayout:
-            canvas.before:
-                Color:
-                    rgba:(0,2/255,46/255,1)
-                    #rgba:(.5,.5,.5,.5)
-                Rectangle:
-                    pos:root.pos
-                    size:root.size
+            Image:
+                source: "./Media/BlueCum.jpg"
+                allow_stretch: True
+                keep_ratio: False
             
+
+<ResultOptionScreen>:
+    name : "resultoption"
+    BoxLayout:
+        orientation: "horizontal"
+        SideBar:
+            size_hint : 0.3,1
+        RelativeLayout:
+            Image:
+                source: "./Media/BlueCum.jpg"
+                allow_stretch: True
+                keep_ratio: False
+            #canvas.before:
+                #Color:
+                    #rgba:(0,2/255,46/255,1)
+                    #rgba:(.5,.5,.5,.5)
+                #Rectangle:
+                    #pos:root.pos
+                    #size:root.size
+            BoxLayout:
+                pos_hint: {"center_x":.5,"center_y":.5}
+                size_hint : .6,.65
+                #padding:[20,20]
+                #spacing:[0,20]
+                orientation:"vertical"
+                canvas.before:
+                    Color:
+                        rgba:(1,1,1,1)
+                    Rectangle:
+                        size:self.size
+                        pos:self.pos
+                RelativeLayout:
+                    MDFillRoundFlatButton:
+                        text: "Predictor"
+                        font_size:"20sp"
+                        theme_text_color: "Custom"
+                        text_color: 1, 1, 1, 1
+                        md_bg_color: (0/255,155/255,133/255,1)
+                        size_hint : 0.7,0.6
+                        pos_hint:{'center_x':.5,'center_y':.5}
+                        on_release:
+                            root.predictor()
+
+                RelativeLayout:
+                    MDFillRoundFlatButton:
+                        text: "Flow Graph"
+                        font_size:"20sp"
+                        theme_text_color: "Custom"
+                        text_color: 1, 1, 1, 1
+                        md_bg_color: (0/255,155/255,133/255,1)
+                        size_hint : 0.7,0.6
+                        pos_hint:{'center_x':.5,'center_y':.5}
+                RelativeLayout:
+                    MDFillRoundFlatButton:
+                        text: "Radii Graph"
+                        font_size:"20sp"
+                        theme_text_color: "Custom"
+                        text_color: 1, 1, 1, 1
+                        md_bg_color: (0/255,155/255,133/255,1)
+                        size_hint : 0.7,0.6
+                        pos_hint:{'center_x':.5,'center_y':.5}  
+
+<PredictorScreen>:
+    name:"predictor"
+    z:z
+    t:t
+    q:q
+    r:r
+    BoxLayout:
+        orientation: "horizontal"
+        SideBar:
+            size_hint : 0.3,1
+        RelativeLayout:
+            Image:
+                source: "./Media/BlueCum.jpg"
+                allow_stretch: True
+                keep_ratio: False
+            #canvas.before:
+                #Color:
+                    #rgba:(0,2/255,46/255,1)
+                    #rgba:(.5,.5,.5,.5)
+                #Rectangle:
+                    #pos:root.pos
+                    #size:root.size
+            BoxLayout:
+                pos_hint: {"center_x":.5,"center_y":.5}
+                size_hint : .6,.65
+                #padding:[20,20]
+                #spacing:[0,20]
+                orientation:"vertical"
+                canvas.before:
+                    Color:
+                        rgba:(1,1,1,1)
+                    Rectangle:
+                        size:self.size
+                        pos:self.pos   
+                GridLayout:
+                    cols:2
+                    rows:4
+                    spacing:[0,20]
+                    padding:[20,20]
+                    size_hint: (1,.8)
+                    ColorLabel:
+                        text:"Location (z)"
+                    TextInput:
+                        id:z
+                        fill_color:(.5,.5,0,1)
+                        #hint_text: "Up stream Radius"
+                        #hint_text_color: (1,0,0,1)
+                    ColorLabel:
+                        text:"Time (t) "
+                    TextInput:
+                        id:t
+                    ColorLabel:
+                        text:"Predicted q"
+                    ColorLabel:
+                        id:q
+                        background_color:(1,1,1,1)
+                        color:(0,0,0,1)
+                        #text:""
+                    ColorLabel:
+                        text:"Predicted R"
+                    ColorLabel:
+                        id:r
+                        background_color:(1,1,1,1)
+                        color:(0,0,0,1)
+                        #text:""
+                RelativeLayout:
+                    size_hint:(1,.2)
+                    MDFillRoundFlatButton:
+                        text:"Compute"
+                        theme_text_color: "Custom"
+                        text_color: 1, 1, 1, 1
+                        md_bg_color: (0/255,155/255,133/255,1)
+                        size_hint : 0.7,0.6
+                        pos_hint:{'center_x':.5,'center_y':.5}
+                        on_release:
+                            root.predict()
+
+                
+                    
+                
+
+       
 """)
 
 class InputScreen(Screen):
@@ -186,8 +312,8 @@ class InputScreen(Screen):
     
     def on_submit(self):
         #create the model object and then pass it to the loading screen
-        head = ex.artery(Ru=float(self.upStr.text), Rd=float(self.downStr.text), timeperiod=float(self.timePeriod.text))
-        LoadingScreen.arteryObj = head
+        """head = ex.artery(Ru=float(self.upStr.text), Rd=float(self.downStr.text), timeperiod=float(self.timePeriod.text))
+        LoadingScreen.arteryObj = head"""
         #print(self.upStr.text)
         app = App.get_running_app()
         app.root.current = "load"
@@ -195,30 +321,43 @@ class InputScreen(Screen):
 class LoadingScreen(Screen):
     arteryObj = None
     def on_enter(self):
-        app = App.get_running_app()
-        app.root.current = "input"
+        """        
         ResultsScreen.model = self.arteryObj.sci_train()
-        ResultsScreen.arteryObj = arteryObj
+        ResultsScreen.arteryObj = arteryObj"""
         app = App.get_running_app()
-        app.root.current = "main"
+        app.root.current = "resultoption"
 
-class ResultScreen(Screen):
+class ResultOptionScreen(Screen):
     arteryObj = None
     model = None
-    pass
+    def predictor(self):
+        PredictorScreen.arteryObj = self.arteryObj
+        app = App.get_running_app()
+        app.root.current = "predictor"
+
+
+class PredictorScreen(Screen):
+    arteryObj = None
+    z  = ObjectProperty(None)
+    t = ObjectProperty(None)
+    r = ObjectProperty(None)
+    q = ObjectProperty(None)
+    def predict(self):
+        #print("predicting")
+        self.r.text="R"
+        self.q.text="Q"
+    
 
 class v1App(MDApp):
     def build(self):
         sm = ScreenManager(transition=CardTransition())
         sm.add_widget(InputScreen())
         sm.add_widget(LoadingScreen())
-        sm.add_widget(ResultScreen)
+        sm.add_widget(ResultOptionScreen())
+        sm.add_widget(PredictorScreen())
         return sm
 
 
 
 if __name__ == "__main__":
     v1App().run()
-
-
-
